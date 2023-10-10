f08a95447c78d3905a56e825c5a59f1633e54873
martin.kirilov <devilmarti@abv.bg>
2023-10-10 13:27:10 +0300

    added sout

--- mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,6 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("newThingAdded");
     }
 }
